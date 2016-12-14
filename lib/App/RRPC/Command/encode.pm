8b6a03559e29332e8e3d1f40c9ff35c25b90c1aa
Ben Herweyer <benjamin.herweyer@gmail.com>
2016-12-14 14:18:00 -0500

    Make Command::encode run on all cpu cores

--- lib/App/RRPC/Command/encode.pm
+++ lib/App/RRPC/Command/encode.pm
@@ -15,19 +15,33 @@ option 'output_dir',
 
 method run {
 	my $sermons = $self->load_metadata;
+	require Parallel::ForkManager;
+	require Sys::CPU;
+
 	my $outdir  = dir($self->output_dir);
 
 	# Validate source files.
 	for my $sermon (@$sermons) {
-		unless ($sermon->has_wav_file) {
-			say 'No source file found for ' . $_->date;
+		unless ($sermon->has_mp3_file) {
+			say 'No source file found for ' . $sermon->identifier;
 			exit 1;
 		}
 	}
 
+	my $pm = Parallel::ForkManager->new(Sys::CPU::cpu_count());
 	for my $sermon (@$sermons) {
-		$sermon->mp3_file->move_to($outdir->file($self->mp3_prefix . $sermon->identifier . '.mp3'));
+		my $pid = $pm->start and next;
+
+		my $mp3_file = $sermon->mp3_file;
+		my $target   = $outdir->file($self->mp3_prefix . $sermon->identifier . '.mp3');
+		if ($mp3_file ne $target) {
+			my $action = $mp3_file->isa('App::RRPC::TempFile') ? 'move_to' : 'copy_to';
+			$mp3_file->$action($target);
+		}
+
+		$pm->finish;
 	}
+	$pm->wait_all_children;
 }
 
 1;
